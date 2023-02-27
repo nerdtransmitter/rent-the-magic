1c04465289b28f0b6027f0698f5cbdcca3a4cee1
Alessia Moison <adm2217@columbia.edu>
2023-02-27 16:49:05 +0100

    dashboard and bookings forms and controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+  def new
+    @costume = Costume.find(params[:costume_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.save
+    redirect_to costume_booking_path(@booking)
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,15 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @costumes = current_user.costumes
+    @bookings = current_user.bookings
+  end
+
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
