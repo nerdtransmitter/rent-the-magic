9782d08911f1d6ba438be5d2d10d69e4b2bf8a19
Alessia Moison <adm2217@columbia.edu>
2023-02-27 17:26:53 +0100

    coded dashboard form logic to create booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,4 @@ class PagesController < ApplicationController
     @costumes = current_user.costumes
     @bookings = current_user.bookings
   end
-
-
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
-  end
 end
