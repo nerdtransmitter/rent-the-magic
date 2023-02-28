60d6d28f530c14603c55065d15c1f00e6556b7d7
Alessia Moison <adm2217@columbia.edu>
2023-02-28 12:54:05 +0100

    pundit and controllers for costume and booking

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,28 @@
+class BookingPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def create?
+    record.user != user
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+end
