b99e7a5fe49d4231728130bbbf88a3f4a2bee72f
Aurore Pinho e Silva <aurore.pinhoesilva@gmail.com>
2023-03-02 11:59:04 +0100

    plusieurs photos

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,7 +2,7 @@ class Costume < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  has_one_attached :photo
+  has_many_attached :photos
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
