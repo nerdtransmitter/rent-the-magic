940e490a145307e53dcd0d9c1d5c55e07142221c
Aurore Pinho e Silva <aurore.pinhoesilva@gmail.com>
2023-03-02 14:59:58 +0100

    search barre ok

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -8,7 +8,7 @@ class Costume < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
 
   include PgSearch::Model
-  pg_search_scope :search_by_name,
+  pg_search_scope :search_by_name_and_category,
   against: [ :name, :category ],
   using: {
     tsearch: { prefix: true } # <-- now `superman batm` will return something!
