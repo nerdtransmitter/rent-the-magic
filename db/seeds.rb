1402efa07751d1a73867f54038a9df4a37eb4419
Olga Rusnac <olga.v.rusnac@gmail.com>
2023-03-03 13:02:08 +0100

    fixe le probleme de map et ajoute de fnt dans la show

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ file = URI.open("https://i.ebayimg.com/images/g/UxQAAOSwDO1j50E1/s-l1600.jpg")
 file1 = URI.open("https://i.ebayimg.com/images/g/VZYAAOSw4jFj50E0/s-l1600.jpg")
 file2 = URI.open("https://i.ebayimg.com/images/g/OJIAAOSwJshj50Ez/s-l1600.jpg")
 costume = Costume.new(
-  name: "Women's Enchanted Unicorn Costume",
+  name: "Women Enchanted Unicorn Costume",
   category: "Unicorn",
   size: "s",
   price: 19.99,
@@ -52,7 +52,7 @@ file3 = URI.open("https://m.media-amazon.com/images/I/51SZnl4bALL._AC_UL1001_.jp
 file4 = URI.open("https://m.media-amazon.com/images/I/51hGlkr7ieL._AC_UL1001_.jpg")
 file5 = URI.open("https://m.media-amazon.com/images/I/51erm1DdR4L._AC_UL1001_.jpg")
 costume3 = Costume.new(
-  name: "Princesses",
+  name: "Princess",
   category: "Character",
   size: "S",
   price: 10.99,
@@ -69,7 +69,7 @@ file4 = URI.open("https://m.media-amazon.com/images/I/510smS7vX8L._AC_UL1001_.jp
 file5 = URI.open("https://m.media-amazon.com/images/I/51OBdwNLN-L._AC_UL1001_.jpg")
 file6 = URI.open("https://m.media-amazon.com/images/I/51l1uSpbrTL._AC_UL1001_.jpg")
 costume4 = Costume.new(
-  name: "Princesses",
+  name: "Princess",
   category: "Character",
   size: "S",
   price: 10.99,
@@ -86,7 +86,7 @@ file7 = URI.open("https://m.media-amazon.com/images/I/71zHdmHLLvL._AC_SL1500_.jp
 file8 = URI.open("https://m.media-amazon.com/images/I/61ygOgvKqsS._AC_SL1500_.jpg")
 
  costume5 = Costume.new(
-  name: "WICH",
+  name: "Wich",
   category: "Wich",
   size: "XS",
   price: 22.99,
@@ -97,5 +97,6 @@ costume5.photos.attach(io: file7, filename: "licorne.png", content_type: "image/
 costume5.photos.attach(io: file8, filename: "licorne.png", content_type: "image/png")
 costume5.user = user
 costume5.save
+
 
 
